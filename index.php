9e1e56d7d1bb408fea4fd3594673cc64673764cd
Homeless_Jester <homelessjetser@gmail.com>
2016-11-07 14:16:54 +0300

    Первые шаги

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+class Article {
+    public $title;
+    public $text;
+
+    public function __construct($title, $text)
+    {
+        $this->title = $title;
+        $this->text = $text;
+    }
+
+    public function view()
+    {
+        echo $this->title;
+        echo '<br>';
+        echo $this->text;
+    }
+}
+
+$a = new Article('Title', 'Text');
+$a->view();
